564f706e241dfacad052972696c64f67f1c81537
mirdaki <mirdaki@users.noreply.github.com>
2019-02-26 13:30:53 -0500

    Initial Maven setup

---
+++ src/test/java/com/codecaptured/SimpleBC/SimpleBCTest.java
@@ -0,0 +1,18 @@
+package com.codecaptured.SimpleBC;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SimpleBCTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
